251aea5bc5a2274b1229fbcf83b5f26a7603f334
Toukir Ahmed Shuva <toukir.ahmed@selise.ch>
2019-03-28 15:52:39 +0600

    Dotnet core chat server and client apps created

---
+++ ChatMainServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using System.Text;
+using System.Linq;
+
+using System.Collections;
+
+namespace ChatMainServer
+{
+    class Program
+    {
+
+        static void MongoConnect(){
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
